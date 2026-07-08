6b7030b216d957c8ab04422679112f7721996a07
Vegetarox <fabianrust187@gmail.com>
2026-07-08 18:08:41 +0200

    Re-enable login and account routes with Netlify auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,39 +11,44 @@ import { RaidDetail } from './pages/RaidDetail';
 import { Roadmap } from './pages/Roadmap';
 import { Codes } from './pages/Codes';
 import { ContentCreators } from './pages/ContentCreators';
+import { AuthPage } from './pages/AuthPage';
+import { AccountPage } from './pages/AccountPage';
 import { PublicUnavailable } from './components/Layout/PublicUnavailable';
+import { AuthProvider } from './lib/auth';
 import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
-    <Router>
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="guides" element={<Guides />} />
-          <Route path="guides/:subcategory" element={<Guides />} />
-          <Route path="guides/detail/:guideId" element={<GuideDetail />} />
-          <Route path="builds" element={<Builds />} />
-          <Route path="builds/:subcategory" element={<Builds />} />
-          <Route path="builds/detail/:buildId" element={<BuildDetail />} />
-          <Route path="collections" element={<Collections />} />
-          <Route path="raids" element={<Raids />} />
-          <Route path="raids/:subcategory" element={<Raids />} />
-          <Route path="raids/detail/:raidId" element={<RaidDetail />} />
-          <Route path="roadmap" element={<Roadmap />} />
-          <Route path="codes" element={<Codes />} />
-          <Route path="creators" element={<ContentCreators />} />
-          <Route path="login" element={<PublicUnavailable />} />
-          <Route path="account" element={<PublicUnavailable />} />
-          <Route path="admin" element={<PublicUnavailable />} />
-          <Route path="builds/submit" element={<PublicUnavailable />} />
-          <Route path="ranking" element={<PublicUnavailable />} />
-          <Route path="druid" element={<PublicUnavailable />} />
-          <Route path="*" element={<PublicUnavailable />} />
-        </Route>
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="guides" element={<Guides />} />
+            <Route path="guides/:subcategory" element={<Guides />} />
+            <Route path="guides/detail/:guideId" element={<GuideDetail />} />
+            <Route path="builds" element={<Builds />} />
+            <Route path="builds/:subcategory" element={<Builds />} />
+            <Route path="builds/detail/:buildId" element={<BuildDetail />} />
+            <Route path="collections" element={<Collections />} />
+            <Route path="raids" element={<Raids />} />
+            <Route path="raids/:subcategory" element={<Raids />} />
+            <Route path="raids/detail/:raidId" element={<RaidDetail />} />
+            <Route path="roadmap" element={<Roadmap />} />
+            <Route path="codes" element={<Codes />} />
+            <Route path="creators" element={<ContentCreators />} />
+            <Route path="login" element={<AuthPage />} />
+            <Route path="account" element={<AccountPage />} />
+            <Route path="admin" element={<PublicUnavailable />} />
+            <Route path="builds/submit" element={<PublicUnavailable />} />
+            <Route path="ranking" element={<PublicUnavailable />} />
+            <Route path="druid" element={<PublicUnavailable />} />
+            <Route path="*" element={<PublicUnavailable />} />
+          </Route>
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
